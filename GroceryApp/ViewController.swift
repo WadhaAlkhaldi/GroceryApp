b733896b5790d951f70f9a64b29542c830640fbb
WadhaAlkhaldi <117339391+WadhaAlkhaldi@users.noreply.github.com>
2023-01-08 09:28:35 +0300

    Initial Commit

---
+++ GroceryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroceryApp
+//
+//  Created by Wadha Alkhaldi on 15/06/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
